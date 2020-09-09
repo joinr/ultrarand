e8bd9e37b0973e15d8c7cfc1e9fea4ec03f1bf2a
tom <tom@that-place>
2020-09-09 12:47:50 -0500

    Exploring array set syntax

--- src/ultrarand/jise.clj
+++ src/ultrarand/jise.clj
@@ -32,7 +32,7 @@
           ^long  l1 (.nextLong tlr)
           ^long  l2 (.nextLong tlr)
           ^chars rt (new [char] 22)]
-      (aset rt 21 (tbl ^int (&  l1 0x3f)))
+      (set! (rt 21) (tbl ^int (&  l1 0x3f)))
       (aset rt 20 (tbl ^int (&  (>>> l1 6) 0x3f)))
       (aset rt 19 (tbl ^int (&  (>>> l1 12) 0x3f)))
       (aset rt 18 (tbl ^int (&  (>>> l1 18) 0x3f)))
@@ -56,6 +56,20 @@
       (aset rt 0  (tbl ^int (&  (>>> l2 60) 0x3f)))
       (String. rt))))
 
+
+^:public
+(defclass Test
+  (def ^:public ^:static ^chars tbl
+    (new [char] [\- \0 \1 \2 \3 \4 \5 \6 \7 \8
+                 \9 \A \B \C \D \E \F \G \H \I
+                 \J \K \L \M \N \O \P \Q \R \S
+                 \T \U \V \W \X \Y \Z \_ \a \b
+                 \c \d \e \f \g \h \i \j \k \l
+                 \m \n \o \p \q \r \s \t \u \v
+                 \w \x \y \z]))
+  ^:public ^:static
+  (defm setChar [^char c]
+    (set! (tbl 0) c)))
 
 ;; public class Rand
 ;; {
